fb40bb33bde550afd97468b4ca16f5929c3f6b47
Lorenzo Bossi <lorenzobossi83@gmail.com>
2015-10-02 16:07:01 -0400

    easier css

--- XKCDTooltip/xkcdtt.user.js
+++ XKCDTooltip/xkcdtt.user.js
@@ -28,28 +28,40 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
+var css = [];
+var i = 0;
+css[i++] = ".xkcdtooltip {";
+css[i++] = "    background-color: #6e7b91; ";
+css[i++] = "    box-shadow: 0 0 5px 0 gray; ";
+css[i++] = "    color: #fff; ";
+css[i++] = "    font-weight: bold; ";
+css[i++] = "    border-color: #071419; ";
+css[i++] = "    border-radius: 12px; ";
+css[i++] = "}";
+css[i++] = ".tooltip { ";
+css[i++] = "    padding: 5px; ";
+css[i++] = "    margin: 2ex auto 1ex; ";
+css[i++] = "    font-size: 70%; ";
+css[i++] = "    width: 60%; ";
+css[i++] = "    border-style: solid; ";
+css[i++] = "    border-width: 1.5px; ";
+css[i++] = "}";
+
 var addAfter = function (dom, newNode){
     dom.parentNode.insertBefore(newNode, dom.nextSibling);
 }
 
-var addTitleBox = function(img, after) {
+var addTitleBox = function(img, after, cssClass) {
       var title = img.title;
       if(title.length == 0)
-	 return;
+	 return null;
       //img.title='';
     
       var titleBox = document.createElement('div');
       titleBox.innerHTML = title;
-      titleBox.classList.add('box');
-      titleBox.style.backgroundColor='#6e7b91';
-      titleBox.style.boxShadow='0 0 5px 0 gray';
-      titleBox.style.padding='5px';
-      titleBox.style.width='60%';
-      titleBox.style.margin='2ex auto 1ex';
-      titleBox.style.fontSize='70%';
-      titleBox.style.fontWeight='bold';
-      titleBox.style.color='#fff';
+      titleBox.classList.add('tooltip');
+      titleBox.classList.add(cssClass);
   
       addAfter(after,titleBox);
       return titleBox;
@@ -60,11 +72,7 @@ window.onload = function() {
   
   if (comicBox) {
       var img = comicBox.getElementsByTagName('img')[0];
-      
-      var titleBox = addTitleBox(img,comicBox);
-    
-    
-    
+      var titleBox = addTitleBox(img, comicBox, 'xkcdtooltip');
       var name = document.getElementById('ctitle').innerHTML;
       var a = document.createElement('a');
       var id = document.location.href.split('/')[3];
@@ -77,8 +85,12 @@ window.onload = function() {
   if(article.length > 0){
     var imgs = article[0].getElementsByTagName('img');
     for (var i =0 ; i<imgs.length ; i++){
-        addTitleBox(imgs[i], imgs[i]);
+        addTitleBox(imgs[i], imgs[i], 'whatiftooltip');
     }
   }
 
-};  
+  var style = document.createElement('style');
+  style.type = 'text/css';
+  style.innerHTML = css.join('\n');
+  document.head.appendChild(style);
+};
